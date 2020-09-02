d3d33a2afce142d405cb74d5e251b7f66d070f58
wangdong <wang10272516@163.com>
2020-09-02 23:57:55 +0800

    feat: 初始化项目结构
    使用cookie_cutter创建项目结构并且使用poetry管理依赖

---
+++ iron_man/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from iron_man.users.models import User
+from iron_man.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
